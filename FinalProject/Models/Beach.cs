58ec1477b11d01b5a9339df658d92035fa6b8ee5
tamaraAbramov <32221985+tamaraAbramov@users.noreply.github.com>
2017-10-10 02:38:00 +0300

    take yuval commits to my branch (#6)
    * Beaches (#4)
    
    * Added beach model
    
    * Added beaches index view
    
    * Delete beachDbConext
    
    * added create view to beaches
    
    * added map location view
    
    * added delete view
    
    * Added top beaches to navigator and load the top 10 beaches from json file (#5)

---
+++ FinalProject/Models/Beach.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Beach
+    {
+        public int BeachId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Zoom { get; set; }
+
+        public double Lattitude { get; set; }
+        
+        public double Longtidute { get; set; }
+
+        public Beach(){
+        }
+        public Beach(int BeachId, string Name, string Description, double Zoom, double Lattitude, double Longtidute){
+            this.BeachId = BeachId;
+            this.Name = Name;
+            this.Description = Description;
+            this.Zoom = Zoom;
+            this.Lattitude = Lattitude;
+            this.Longtidute = Longtidute;
+        }
+    }
+}
