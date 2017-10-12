8982247a10dcaa7fa3555c97003c7c896c78d6b7
Tamara Abramov <abramovtam9288@gmail.com>
2017-10-12 14:41:32 +0300

    articles result list

--- FinalProject/Controllers/NewsController.cs
+++ FinalProject/Controllers/NewsController.cs
@@ -1,6 +1,7 @@
 ﻿using FinalProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,7 +19,7 @@ namespace FinalProject.Controllers
             if (User.IsInRole("NormalUser") || User.IsInRole("Admin") ||
                 User.IsInRole("Author"))
             {
-                var orderedArticles = db.Articles.OrderByDescending(a => a.PublishDate);
+                var orderedArticles = db.Articles.OrderByDescending(a => a.SearchCount).ThenByDescending(a => a.PublishDate);
                 return View(orderedArticles.ToList());
             }
             else
@@ -33,11 +34,59 @@ namespace FinalProject.Controllers
         }
 
 
-        public string SearchArticleResult(string articleTitle, string autherName, string articleText,
+        public PartialViewResult SearchArticleResult(string articleTitle, string autherName, string articleText,
           DateTime? startDate, DateTime? endDate)
         {
+            string whereQuery = String.Empty;
 
-            return "WORK";
+            if(articleTitle == DEFULT_RESULT)
+            {
+                articleTitle = string.Empty;
+            }
+
+            if (autherName == DEFULT_RESULT)
+            {
+                autherName = string.Empty;
+            }
+
+            if (articleText == DEFULT_RESULT)
+            {
+                articleText = string.Empty;
+            }
+
+            if (startDate == null)
+            {
+                startDate = DateTime.MinValue;
+            }
+
+            if (endDate == null)
+            {
+                endDate = DateTime.MaxValue;
+            }
+
+            var articles = from a in db.Articles
+                           where a.Title.Contains(articleTitle)
+                           where a.Author.Contains(autherName)
+                           where a.Text.Contains(articleText)
+                           where a.PublishDate >= startDate
+                           where a.PublishDate <= endDate
+                           select a;
+
+            UpdateStatisticResult(articles.ToList());
+
+            return PartialView("../Shared/_ArticlesResult", articles.ToList());
+        }
+
+        private void UpdateStatisticResult(List<Article> articles)
+        {
+            foreach (Article article in articles)
+            {
+                article.SearchCount++;
+
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            
         }
     }
 }
